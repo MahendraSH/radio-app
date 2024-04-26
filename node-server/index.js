d3e3ee88ad2fd52a3f55383fd67fc74428d2a49f
MahendraSH <maahi2001sh@gmail.com>
2024-04-26 15:04:20 +0530

    cookie doamin

--- node-server/index.js
+++ node-server/index.js
@@ -16,8 +16,8 @@ const app = express();
 
 
 app.use(cors({
-    // origin: 'https://radio-app-demo.vercel.app',
-    origin: 'http://localhost:3000',
+    origin: 'https://radio-app-demo.vercel.app',
+    // origin: 'http://localhost:3000',
 
     credentials: true
 }));
